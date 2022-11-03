3c06ea7e85b7a826319603e94a3a098e13f36836
m2rk <1312@duck.com>
2022-11-03 23:16:22 +0100

    yes

--- src/preload.js
+++ src/preload.js
@@ -7,28 +7,3 @@ document.addEventListener("keydown", (event) => {
     document.exitPointerLock();
   }
 })
-
-window.prompt = () => { // import settings fix
-  var tempHTML = '<div class="setHed">Import Settings</div>';
-  tempHTML +=
-      '<div class="settName" id="importSettings_div" style="display:block">Settings String<input type="url" placeholder="Paste Settings String Here" name="url" class="inputGrey2" id="settingString"></div>';
-  tempHTML += '<a class="+" id="importBtn">Import</a>';
-  menuWindow.innerHTML = tempHTML;
-  importBtn.addEventListener('click',
-                             () => { parseSettings(settingString.value); });
-
-  function parseSettings(string) {
-    if (string && string != '') {
-      try {
-        var json = JSON.parse(string);
-        for (var setting in json) {
-          setSetting(setting, json[setting]);
-          showWindow(1);
-        }
-      } catch (err) {
-        console.error(err);
-        alert('Error importing settings.');
-      }
-    }
-  }
-};
