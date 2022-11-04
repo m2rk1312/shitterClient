6fb08ed85b1a8bb6c1c6aa98f49f2362d75c6916
m2rk <1312@duck.com>
2022-11-04 16:32:49 +0100

    Removed yarn bozo trash n added F6 hotkey

--- README.md
+++ README.md
@@ -2,21 +2,29 @@
  
 bro thinks hes tough talking on discord 😭 if you ever see me in the streets you better run because i dont play no games.
 
-build tools:
-```
-git
-nodejs
-yarn
-```
-download/run instructions:
-```sh
-git clone https://github.com/m2rk1312/shitterClient.git
-cd shitterClient
-yarn #install deps
-yarn start #run client
-```
-if you want to compile it, just run `yarn package-<PLATFORM>` but with your platform, ex: `yarn package-linux`
+# Hotkeys
+- F11, toggle fullscreen
+- F5, refresh page
+- F6, join link stored in clipboard
 
+# Build instructions
+```sh
+Install Git and Nodejs
 ```
-Fork of JANREX - https://github.com/giantninja908/JANREX
+```sh
+git clone https://github.com/m2rk1312/shitterClient
+```
+```sh
+cd shitterClient
+```
+```sh
+npm i
+```
+If you just want to run the program from console/terminal:
+```sh
+npm start
+```
+If you want to build it:
+```sh
+npm run dist
 ```
--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
-// Fork of JANREX - https://github.com/giantninja908/JANREX
-
-const { app, BrowserWindow, screen } = require("electron");
+const { app, BrowserWindow, screen, clipboard } = require("electron");
 const path = require('path');
-require('v8-compile-cache');
-
 const { shell } = require('electron');
+require('v8-compile-cache');
 
 let win;
 
@@ -28,7 +25,9 @@ const init =
     win.webContents.on('before-input-event', (event, input) => {
       if (input.key === "F11") { win.setFullScreen(!win.isFullScreen()); event.preventDefault() }
       if (input.key === "F5") { win.reload(); event.preventDefault() }
+      if (input.key === "F6") { win.loadURL(clipboard.readText()); event.preventDefault() }
     });
+
     win.on('close', () => { app.quit() });
     win.webContents.on('new-window', (event, url, _, __, options) => {
       if (!url)
@@ -61,34 +60,33 @@ const init =
     });
   }
 
-const addSwitches =
-  () => {
-      app.commandLine.appendSwitch("force_high_performance_gpu");
-      app.commandLine.appendSwitch("force-high-performance-gpu");
-      app.commandLine.appendSwitch("disable-breakpad");
-      app.commandLine.appendSwitch("disable-component-update");
-      app.commandLine.appendSwitch("disable-print-preview");
-      app.commandLine.appendSwitch("disable-metrics");
-      app.commandLine.appendSwitch("disable-metrics-repo");
-      app.commandLine.appendSwitch("enable-javascript-harmony");
-      app.commandLine.appendSwitch("enable-future-v8-vm-features");
-      app.commandLine.appendSwitch("enable-webgl2-compute-context");
-      app.commandLine.appendSwitch("disable-hang-monitor");
-      app.commandLine.appendSwitch("no-referrers");
-      app.commandLine.appendSwitch("renderer-process-limit", 100);
-      app.commandLine.appendSwitch("max-active-webgl-contexts", 100);
-      app.commandLine.appendSwitch("enable-quic");
-      app.commandLine.appendSwitch("high-dpi-support", 1);
-      app.commandLine.appendSwitch("ignore-gpu-blacklist");
-      app.commandLine.appendSwitch("disable-2d-canvas-clip-aa");
-      app.commandLine.appendSwitch("disable-bundled-ppapi-flash");
-      app.commandLine.appendSwitch("disable-logging");
-      app.commandLine.appendSwitch("disable-web-security");
-      app.commandLine.appendSwitch("webrtc-max-cpu-consumption-percentage=100");
-      app.commandLine.appendSwitch('disable-frame-rate-limit');
-      app.commandLine.appendSwitch("disable-gpu-vsync");
-      app.commandLine.appendSwitch("in-process-gpu");
-  }
+const addSwitches = () => {
+  app.commandLine.appendSwitch("force_high_performance_gpu");
+  app.commandLine.appendSwitch("force-high-performance-gpu");
+  app.commandLine.appendSwitch("disable-breakpad");
+  app.commandLine.appendSwitch("disable-component-update");
+  app.commandLine.appendSwitch("disable-print-preview");
+  app.commandLine.appendSwitch("disable-metrics");
+  app.commandLine.appendSwitch("disable-metrics-repo");
+  app.commandLine.appendSwitch("enable-javascript-harmony");
+  app.commandLine.appendSwitch("enable-future-v8-vm-features");
+  app.commandLine.appendSwitch("enable-webgl2-compute-context");
+  app.commandLine.appendSwitch("disable-hang-monitor");
+  app.commandLine.appendSwitch("no-referrers");
+  app.commandLine.appendSwitch("renderer-process-limit", 100);
+  app.commandLine.appendSwitch("max-active-webgl-contexts", 100);
+  app.commandLine.appendSwitch("enable-quic");
+  app.commandLine.appendSwitch("high-dpi-support", 1);
+  app.commandLine.appendSwitch("ignore-gpu-blacklist");
+  app.commandLine.appendSwitch("disable-2d-canvas-clip-aa");
+  app.commandLine.appendSwitch("disable-bundled-ppapi-flash");
+  app.commandLine.appendSwitch("disable-logging");
+  app.commandLine.appendSwitch("disable-web-security");
+  app.commandLine.appendSwitch("webrtc-max-cpu-consumption-percentage=100");
+  app.commandLine.appendSwitch('disable-frame-rate-limit');
+  app.commandLine.appendSwitch("disable-gpu-vsync");
+  app.commandLine.appendSwitch("in-process-gpu");
+}
 
 addSwitches();
 app.on('ready', init);
